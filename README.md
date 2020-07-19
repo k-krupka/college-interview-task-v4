63146b3b328d24b30d93776ee49bb097951ea668
Jakub Krupka <jakub.krupka@pl.abb.com>
2020-07-19 11:47:58 +0200

    start

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-# college-interview-task-v4
+﻿# Zadanie rekrutacyjne
+
+## Wstęp
+
+Zawartość służy jako test rekrutacyjny dla studentów do projektu informatycznego na stanowisko "programista .net".
+
+Wymagane umiejętności by ukończyć zadanie:
+
+1. znajomość języka programowania C#
+2. pozytywne nastawienie
+
+Dystrybucja:
+
+* wszystkie zadania należy umieścić w prywatnym repo kandydata i przesłać link w odpowiedzi na maila rekrutacyjnego
+
+Informacje dodatkowe:
+
+* nie trzeba przynosić komputera na rozmowę rekrutacyjną (jeśli odbędzie się w biurze ze względu na wyjątkową sytuację w kraju)
+* podczas rozmowy rekrutacyjnej pojawią się dodatkowe pytania w sprawie wykonanego zadania
+
+## Zadanie 1 - co to jest (analiza kodu źródłowego)
+
+W tym zadaniu należy słownie, opisać kod źródłowy którego dotyczy rekrutacja (patrz zadanie 2 i 3)
+
+1. co ten kod robi
+2. jakie widać problemy
+3. co jest fajnego?
+4. jakie widzimy niebezpieczeństwa używając tej metody?
+
+## Zadanie 2 - refactoring & unit test
+
+W tym zadaniu należy:
+
+1. zrobić refactoring klasy **HttpRequestHandler.cs**, aby była:
+   * bardziej przejrzysta do czytania
+   * lepsza w używaniu
+2. zaimplementować wszystkie interfejsy, przygotować konkretną implementację klasy bazowej, itd.
+3. napisać unit testy do przykładowej klasy aby pokazać "że działa"
+
+**Uwaga:**
+
+* niniejsza klasa została zmodyfikowana celowo aby uzyskać efekt "bad design" / "bad quality" na potrzeby zadania rekrutacyjnego
+* _"wszystkie chwyty dozwolone"_
+* można w dowolny sposób zmieniać zawartość klasy, jej konstruktory, sygnatury metod, itp.
+
+## Zadanie 3 - demo (sposób użycia)
+
+W tym zadaniu należy przygotować, w dowolny sposób, krótką prezentację wykorzystania klasy **HttpRequestHandler.cs** z poprzednich zadań. Może to być zrobione w formie aplikacji konsolowej (zalecane), webowej lub dowolnej innej formie.
+
+**Uwaga:**
+
+1. demo powinno być przygotowane i wrzucone do nowego repozytorium GitHub kandydata
+   * kandydat wysyła link do swojego GitHub w celu weryfikacji poprawności wykonania zadania
+
+## Podsumowanie
+
+To już koniec "zadania domowego" :) Czy na koniec, możemy uprzejmie prosić o jedno zdanie komentarza na temat powyższych trzech zadań?
---
+++ college-interview-task-v4/HttpRequestHandler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace college_interview_task_v4
+{
+    public abstract class HttpRequestHandler<TRequest, TResponse>
+    {
+        public const string Variable1 = "abc";
+
+
+
+        internal HttpClient _httpClientProxy;
+
+        private IHttpResponseParser<TResponse> _parser { get; }
+
+
+
+        protected HttpRequestHandler(HttpClient httpClientProxy, IHttpResponseParser<TResponse> parser)
+        {
+            _parser = parser;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, object payload, HttpMethod methodName,
+            string pleaseProvideFullUrlHere,
+            IDictionary<string, string> additionalHeaders, CancellationToken cancellationToken)
+        {
+            var uriString = $"{_httpClientProxy.BaseAddress}{pleaseProvideFullUrlHere}";
+
+            var httpRequestMessage = new HttpRequestMessage(methodName, new Uri(uriString));
+
+            if (payload == null)
+            {
+            }
+            else
+            {
+                httpRequestMessage.Content = payload as HttpContent;
+            }
+
+            var headers = additionalHeaders;
+
+            if (headers == null)
+            {
+            }
+            else
+            {
+                foreach (var header in headers)
+                {
+                    httpRequestMessage.Headers.Add(header.Key, header.Value);
+                }
+            }
+
+            HttpResponseMessage response = await _httpClientProxy.SendAsync(httpRequestMessage, cancellationToken);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new EndOfStreamException(string.Join(",", response.Headers));
+            }
+
+            try
+            {
+                return _parser.ParseAsync(response);
+            }
+            catch (Exception ex)
+            {
+                throw new NotImplementedException("todo: add missing implementation");
+            }
+        }
+
+        public interface IHttpResponseParser<out TResult>
+        {
+            TResult ParseAsync(HttpResponseMessage response);
+        }
+
+
+    }
+}
